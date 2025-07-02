59dea1ce6a063fed3e360833fbe74495685ec3f9
Anish Raj <anishrajdmk@gmail.com>
2025-07-02 16:20:09 +0530

    Fixed the font size

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -11,9 +11,9 @@ const Header=()=>{
     }
     return(
         <div className={`bg-Background w-full h-20 border-b-1  border-divider flex justify-between px-7 lg:px-16 items-center gap-5`}>
-            <h1 className={`text-ptext text-xl md:text-2xl font-bold Gilroy`}>Score -  <span>{score}</span></h1>
+            <h1 className={`text-ptext text-md md:text-2xl font-bold Gilroy`}>Score -  <span>{score}</span></h1>
             <button onClick={handleReset} className={`text-md px-4 py-2 rounded full bg-divider text-stext font-medium `}>Reset</button>
-            <h1  className={`text-ptext text-xl md:text-2xl font-bold Gilroy`}>Highest  -  <span>{highest}</span></h1>
+            <h1  className={`text-ptext text-md md:text-2xl font-bold Gilroy`}>Highest  -  <span>{highest}</span></h1>
         </div>
     )
 }
