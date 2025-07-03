c750f473c12d75bd0925e7372ca854f5a3f0e421
Anish Raj <anishrajdmk@gmail.com>
2025-07-03 10:31:44 +0530

    Fixed the input outside the limits.

--- src/Pages/GameStart.tsx
+++ src/Pages/GameStart.tsx
@@ -1,4 +1,4 @@
-import {type FormEvent, useState} from "react";
+import {type ChangeEvent, type FormEvent, useState} from "react";
 import {useGameStore} from "../Store/useGameStore.ts";
 import {useShallow} from "zustand/react/shallow";
 import {useNavigate} from "react-router-dom";
@@ -31,19 +31,44 @@ const itemVarient={
 const GameStart=()=>{
     const {startGame}=useGameStore(useShallow(state=>({startGame:state.startGame})));
     const [prob, setProb] = useState<number|undefined>(80);
+    const [isError, setIsError] = useState<boolean>(false);
+    const [errorMsg, setErrorMsg] = useState<string>('')
     const navigate=useNavigate();
     const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
-        startGame(prob!);
+        startGame(Number(prob!));
         navigate('/game');
+    }
+    const handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
+        const val=Number(e.target.value);
+        setProb(val);
+        if(val>=0&&val<=100){
+            setIsError(false);
+            setErrorMsg('');
+        }else{
+            setIsError(true);
+            setErrorMsg('Enter value between 0 and 100');
+        }
+
     }
     return(
         <div className={`w-full h-screen bg-Background `}>
             <Header/>
             <motion.form initial={'hidden'} animate={'visible'} variants={containerVarient} className={`flex flex-col  items-center justify-center gap-4 h-[80vh]`} onSubmit={(e=>handleSubmit(e))}>
                 <motion.label variants={itemVarient!} htmlFor="input" className={`text-ptext font-medium Gilroy text-center text-xl italic`}>Enter the Probability</motion.label>
-                <motion.input variants={itemVarient} value={prob}  onChange={(e)=>setProb(Number(e.target.value))} type="number" className={`Gilroy text-2xl placeholder:text-sm text-center text-ptext px-4 p-2 w-1/3 rounded-xl  font-medium bg-divider/80 focus:outline-none focus:ring-2 focus:ring-accent  outline-none lg:w-2/15 no-spinners`} placeholder={`Enter probability`}/>
-                <motion.button variants={itemVarient} disabled={prob === undefined} className={`text-ptext Gilroy px-4 py-2 rounded-lg bg-accent outline-2 outline-accent active:outline-divider disabled:text-stext`}>Submit</motion.button>
+                <motion.input variants={itemVarient} value={prob}  onChange={handleChange} type="number" className={`Gilroy text-2xl placeholder:text-sm text-center text-ptext px-4 p-2 w-1/3 rounded-xl  font-medium bg-divider/80 focus:outline-none focus:ring-2 focus:ring-accent  outline-none lg:w-2/15 no-spinners relative`} placeholder={`Enter probability`}/>
+                <motion.div className={`h-4`}>
+                    {isError && errorMsg && (
+                        <motion.p
+                            variants={itemVarient}
+                            className="text-red-500 text-sm font-medium italic whitespace-nowrap"
+                        >
+                            {errorMsg}
+                        </motion.p>
+                    )}
+                </motion.div>
+
+                <motion.button variants={itemVarient} disabled={prob === undefined||isError} className={`text-ptext Gilroy px-4 py-2 rounded-lg bg-accent outline-2 outline-accent active:outline-divider disabled:text-stext`}>Submit</motion.button>
             </motion.form>
         </div>
     );
